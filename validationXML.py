f8b72b9ce3541e8feae08a4ebaba812df968edcf
Maïmouna Nimbert Coly <46159900+Nimbert-Coly@users.noreply.github.com>
2019-01-16 19:24:53 +0100

    Update validationXML.py

--- validationXML.py
+++ validationXML.py
@@ -2,14 +2,16 @@ from lxml import etree
 from io import StringIO
 import sys
 
-filename_xml = 'monfichier.xml'
+def xmlvalide(monfichier):
 
-with open(filename_xml, 'r') as xml_file:
-    xml_to_check = xml_file.read()
+	with open(monfichier, 'r') as xml_file:
+    	xml_to_check = xml_file.read()
 
-try:
-    doc = etree.parse(StringIO(xml_to_check))
-    print('Fichier valide.Syntax ok.')
+	try:
+    	doc = etree.parse(StringIO(xml_to_check))
+    	#print('Fichier valide.Syntax ok.')
+    	return True
 
-except:
-    print('Fichier invalide. Syntax incorrect.')
+	except:
+    	print('Fichier invalide. Syntax incorrect.')
+    	return False
